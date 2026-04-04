d13bebef6295fb608f8d3f9a41bdcf52e958d749
Jet <angel0607buen@gmail.com>
2026-04-04 23:53:19 +0800

    Create config.js for MongoDB connection settings
    Added configuration for MongoDB connection URI.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	MONGO_URI: "change to personal URI"
+};
+
+// connect using drivers and use the NON-SRV string.
+// there should be one question mark in the entire link.
+// add the db name after the slash and before the question mark (mongodb.net:27017/reservation_db?)

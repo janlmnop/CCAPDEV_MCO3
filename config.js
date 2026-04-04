79a670a58e999ffc467562dd72135de00a98eebc
Jet <angel0607buen@gmail.com>
2026-04-05 01:02:49 +0800

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	MONGO_URI: "change to personal URI"
-};
-
-// connect using drivers and use the NON-SRV string.
-// there should be one question mark in the entire link.
-// add the db name after the slash and before the question mark (mongodb.net:27017/reservation_db?)

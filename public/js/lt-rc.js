30002bd50210b3ff5bd7dd2ef1a4acfe0f74bd7a
Jet <angel0607buen@gmail.com>
2026-04-07 01:39:37 +0800

    open profile when clicking student name

--- public/js/lt-rc.js
+++ public/js/lt-rc.js
@@ -716,7 +716,14 @@ function fillSchedulePage() {
         const selectable = CanSelectSlot(currentScheduleDate, slotIndex);
 
         if (reservedInfo) {
-            slotCell.textContent = reservedInfo.reservedBy || "Reserved";
+			const displayName = reservedInfo.reservedBy || "Reserved";
+
+			if (reservedInfo.userId) {
+				slotCell.innerHTML = "<a href="/views/other-student/userprofile_student_other.html?student_id=${reservedInfo.userId}">
+                ${displayName}</a>";
+			} else {
+				slotCell.textContent = displayName
+			}
         } else {
             slotCell.innerHTML = "";
         }
